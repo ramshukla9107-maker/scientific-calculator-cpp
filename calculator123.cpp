d2fc2b4e99b8b0a8d171c7b7d789e524fd72e9ca
Ram Kushal Shukla <ramshukla9107@gmail.com>
2026-03-17 07:37:32 +0530

    Update calculator123.cpp with new calculator code

--- calculator123.cpp
+++ calculator123.cpp
@@ -4,12 +4,14 @@ int main (){
     int choice;
     double num1,num2;
     do{
-        cout<<"\n=====SIMPLE CALCULATOR=====\n";
+        cout<<"\n=====SIMPLE CALCULATOR=====
+";
         cout<<"1.addition(+) \n";
         cout<<"2.substraction(-) \n";
         cout<<"3.multiplication(*) \n";
         cout<<"4.division (/) \n";
-        cout<<"5.enter your choice:";
+        cout<<"5.exit\n";
+        cout<<"enter your choice:";
         cin>>choice;
         if(choice>=1 && choice<=4){
             cout<<"enter first number";
@@ -20,20 +22,23 @@ int main (){
             switch (choice){
                 case 1:
                 cout<<"result="<<num1 + num2<<endl;
+                break;
                 case 2:
                 cout<<"result="<<num1 - num2<<endl;
+                break;
                 case 3:
                 cout<<"result="<<num1 * num2<<endl;
+                break;
                 case 4:
                 if (num2!=0)
                 cout<<"result="<<num1 / num2<<endl;
-                else cout<<"error!division by zero not allowed>\n";
+                else cout<<"error!division by zero not allowed\n";
                 break;
                 case 5:
                 cout<<"thankyou for using calculator\n";
                 break;
                 default:
-                cout<<"invalid choice!try again>\n";
+                cout<<"invalid choice!try again\n";
             }
     }while (choice!=5);
     return 0;
